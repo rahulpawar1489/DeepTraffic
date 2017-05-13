4e296e3fba5453a75d96d97026d4cb29f4b748b7
rahulpawar1489 <rahulpawar1489@gmail.com>
2017-05-13 15:25:01 +0530

    Add files

---
+++ 74.73.js
@@ -0,0 +1,65 @@
+//<![CDATA[
+
+// a few things don't have var in front of them - they update already existing variables the game needs
+lanesSide = 3;
+patchesAhead = 30;
+patchesBehind = 3;
+trainIterations = 150000;
+
+var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);
+var num_actions = 5;
+var temporal_window = 0;
+var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;
+
+var layer_defs = [];
+layer_defs.push({
+    type: 'input',
+    out_sx: 1,
+    out_sy: 1,
+    out_depth: network_size
+});
+layer_defs.push({
+    type: 'fc',
+    num_neurons: 196,
+    activation: 'relu'
+});
+layer_defs.push({
+    type: 'fc',
+    num_neurons: 196,
+    activation: 'relu'
+});
+layer_defs.push({
+    type: 'regression',
+    num_neurons: num_actions
+});
+
+var tdtrainer_options = {
+    method: 'adagrad',
+    batch_size: 150,
+    //l1_decay: 0.004,
+    l2_decay: 0.002
+};
+
+var opt = {};
+opt.temporal_window = temporal_window;
+opt.experience_size = 30000;
+opt.start_learn_threshold = 5000;
+opt.gamma = 0.95;
+opt.learning_steps_total = 20000;
+opt.learning_steps_burnin = 1000;
+opt.epsilon_min = 0.02;
+opt.epsilon_test_time = 0.04;
+opt.layer_defs = layer_defs;
+opt.tdtrainer_options = tdtrainer_options;
+
+brain = new deepqlearn.Brain(num_inputs, num_actions, opt);
+
+learn = function (state, lastReward) {
+    brain.backward(lastReward);
+    var action = brain.forward(state);
+
+    draw_net();
+    draw_stats();
+
+    return action;
+}
